6d03dabfc34cd853eeeab4e38d450e3bcf19f780
Dmitry Petrov <dvpetrov.main@gmail.com>
2018-12-06 23:11:08 +0300

    Upgrade About page

--- packages/about/src/containers/About/About.js
+++ packages/about/src/containers/About/About.js
@@ -1,4 +1,6 @@
 import React, { PureComponent } from 'react'
+import { Link } from 'react-router-dom'
+// import Response from '@rispa/render-server/response'
 // import { string, number, bool, array, func, node } from 'prop-types'
 
 export default class About extends PureComponent {
@@ -16,6 +18,10 @@ export default class About extends PureComponent {
     return (
       <div>
         About
+        <br />
+        <Link to='/'>
+          Go to general page
+        </Link>
       </div>
     )
   }
